8a5f0adbbb884f6e00bf432ed55fd3287ccd7601
mrdbp <mrgoodmrevil@gmail.com>
2021-11-20 17:15:26 +0800

    Ini file pertamaku untu GitHub

---
+++ index.js
@@ -0,0 +1,8 @@
+const nama = "MR.DBP"; //cont mutlak
+let hobi = "Rakit PC"; //let bisa diganti isinya
+// hobi = "baca manhwa solo leveling"; 
+hobi = "nama saya ${nama} dan hobi"; 
+
+console.log(`${nama} ${hobi}`);
+//ctrl shift p
+// munculin terminal ctrl ~
